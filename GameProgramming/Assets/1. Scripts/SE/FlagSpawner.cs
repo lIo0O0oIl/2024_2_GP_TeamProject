47a1f55d5b674eae43937a04603e1ccfecf6a09e
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-08-05 14:43:08 +0900

    깃발 제작중 (1)
    - 게임 오브젝트 클릭 시 디버그
    - 닷트윈 추가

---
+++ GameProgramming/Assets/1. Scripts/SE/FlagSpawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject flagPrefab;
+    [SerializeField] private string flagColorList;
+
+
+}

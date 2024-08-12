bba7bc73435f2fe1e1fc62fb6096b5d23b62b2c1
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-08-12 12:09:42 +0900

    깃발 제작중 (2)
    - 깃발 올라갔다가 내려왔다가 하게 만들어줌.
    - 색을 바로 바뀌게 만들어줌.
    - 이제 닷트윈을 속도에 따라 움직이게 해줘야함. (지금은 1초동안 이동하게 만듦.)

--- GameProgramming/Assets/1. Scripts/SE/Flag.cs
+++ GameProgramming/Assets/1. Scripts/SE/Flag.cs
@@ -1,9 +1,71 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Flag : MonoBehaviour
 {
-    public string color;
+    public string colorName;
+    public Color color;
 
+    [SerializeField] private Vector2 orginPos;
+    [SerializeField] private Vector2 downPos;
+
+    private bool is_up = false;
+    private GameObject cloth;
+    private SpriteRenderer clothSpriteRenderer;
+
+    private void OnValidate()
+    {
+        if (cloth == null)
+        {
+            cloth = transform.GetChild(1).gameObject;
+            if (clothSpriteRenderer == null)
+            {
+                clothSpriteRenderer = cloth.GetComponent<SpriteRenderer>();
+                clothSpriteRenderer.color = color;
+            }
+            else
+            {
+                clothSpriteRenderer.color = color;
+            }
+        }
+        else
+        {
+            if (clothSpriteRenderer == null)
+            {
+                clothSpriteRenderer = cloth.GetComponent<SpriteRenderer>();
+                clothSpriteRenderer.color = color;
+            }
+            else
+            {
+                clothSpriteRenderer.color = color;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        if (color == null)
+        {
+            cloth = transform.GetChild(1).gameObject;
+        }
+        orginPos = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"{colorName}색 깃발 클릭");
+        is_up = !is_up;
+        if (is_up )
+        {
+            DOTween.Kill(cloth);
+            cloth.transform.DOMove(downPos, 1f).SetEase(Ease.Linear);
+        }
+        else
+        {
+            DOTween.Kill(cloth);
+            cloth.transform.DOMove(orginPos, 1f).SetEase(Ease.Linear);
+        }
+    }
 }

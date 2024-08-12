efd2b1d6a982c4b080dde2ccc77a4ef8c5e336fc
CAFUCH <tjfdk6201@gmail.com>
2024-08-12 11:33:54 +0900

    명령어, 타이머, 카메라 기본 기능 개발

---
+++ GameProgramming/Assets/1. Scripts/SA/CameraMove.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject background;
+
+    private Camera mainCamera;
+    private float halfHeight;
+    private float halfWidth;
+    public float speed;
+
+    private float minX, maxX, minY, maxY;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        halfHeight = mainCamera.orthographicSize;
+        halfWidth = mainCamera.aspect * halfHeight;
+
+        SpriteRenderer backgroundSprite = background.GetComponent<SpriteRenderer>();
+        float backgroundWidth = backgroundSprite.bounds.size.x;
+        float backgroundHeight = backgroundSprite.bounds.size.y;
+
+        minX = background.transform.position.x - backgroundWidth / 2f + halfWidth;
+        maxX = background.transform.position.x + backgroundWidth / 2f - halfWidth;
+        minY = background.transform.position.y - backgroundHeight / 2f + halfHeight;
+        maxY = background.transform.position.y + backgroundHeight / 2f - halfHeight;
+    }
+
+    void LateUpdate()
+    {
+        MoveCamera();
+        ClampCamera();
+    }
+
+    private void MoveCamera()
+    {
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        mainCamera.transform.Translate(new Vector3(moveX, moveY, 0f));
+    }
+
+    private void ClampCamera()
+    {
+        Vector3 camera = mainCamera.transform.position;
+
+        float clampedX = Mathf.Clamp(camera.x, minX, maxX);
+        float clampedY = Mathf.Clamp(camera.y, minY, maxY);
+
+        mainCamera.transform.position = new Vector3(clampedX, clampedY, mainCamera.transform.position.z);
+    }
+
+
+    //[SerializeField] private float speed;
+
+    //private void Update()
+    //{
+    //    float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+    //    float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+    //    Camera.main.transform.Translate(new Vector3(moveX, moveY, 0f));
+    //}
+
+    //private void clamp()
+    //{
+
+    //}
+}
